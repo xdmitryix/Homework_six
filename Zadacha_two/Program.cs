276f82fc814ee3b39c0244bff97b06b940813fbf
dmitry <rbdeman@yandex.ru>
2022-12-11 18:50:56 +0300

    Исправил задачу 2

--- Zadacha_two/Program.cs
+++ Zadacha_two/Program.cs
@@ -2,47 +2,23 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-
-// void FinalResult(int B1, int K1, int B2, int K2)
-// {
-//     int y1= 
-// }
-
-
-
-
-
-
-
-
-// Consolr.Clear();
-// Console.WriteLine("введи значение b1: ");
-// int b1 = int.Parse(Console.WriteLine());
-// Console.WriteLine("введи значение k1: ");
-// int k1 = int.Parse(Console.WriteLine());
-// Console.WriteLine("введи значение b2: ");
-// int b2 = int.Parse(Console.WriteLine());
-// Console.WriteLine("введи значение k2: ");
-// int k2 = int.Parse(Console.WriteLine());
-
-
-Console.Write("Введите k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+double x = - (b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
 x = Math.Round(x, 3);
 y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
 
 
 
